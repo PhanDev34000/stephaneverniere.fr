36a41d46c2724a6d7df018c4dbf314789feb3f1c
PhanDev34000 <vernierestephane@hotmail.com>
2025-09-11 18:05:35 +0200

    MAJ ficheir environment 2

--- frontend/site-photo/src/environments/environment.ts
+++ frontend/site-photo/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  API_BASE: 'https://stephaneverniere-fr.vercel.app/api',
+  API_BASE: 'http://localhost:3000/api',
   DISABLE_BACKEND: false,
 };
