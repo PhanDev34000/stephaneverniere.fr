842dbce72543b8c6f6d72545e4cce54eaccf9395
PhanDev34000 <vernierestephane@hotmail.com>
2025-09-11 18:27:58 +0200

    MAJ R2js

--- backend/src/services/r2.js
+++ backend/src/services/r2.js
@@ -1,9 +1,9 @@
-// src/services/r2.js
-const { S3Client, PutObjectCommand, ListObjectsV2Command, HeadBucketCommand } = require('@aws-sdk/client-s3');
+const { S3Client, PutObjectCommand, ListObjectsV2Command } = require('@aws-sdk/client-s3');
 
 const client = new S3Client({
   region: 'auto',
   endpoint: process.env.R2_ENDPOINT,
+  forcePathStyle: true, // important avec R2
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
@@ -12,7 +12,7 @@ const client = new S3Client({
 
 const Bucket = process.env.R2_BUCKET;
 
-exports.ping = () => client.send(new HeadBucketCommand({ Bucket }));
+exports.ping = () => client.send(new ListObjectsV2Command({ Bucket, MaxKeys: 1 }));
 exports.put  = (Key, Body, ContentType='application/octet-stream') =>
   client.send(new PutObjectCommand({ Bucket, Key, Body, ContentType }));
 exports.list = (Prefix) => client.send(new ListObjectsV2Command({ Bucket, Prefix }));
