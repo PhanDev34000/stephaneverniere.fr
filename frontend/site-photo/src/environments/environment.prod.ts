1e2a1364801712e094d0769a9ab5121fa2e7fbaa
PhanDev34000 <vernierestephane@hotmail.com>
2025-09-11 18:05:35 +0200

    MAJ ficheir environment 2

--- frontend/site-photo/src/environments/environment.prod.ts
+++ frontend/site-photo/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_BASE: 'https://stephaneverniere-fr.vercel.app/api',
+  API_BASE: 'https://symbolic-rubia-svdev-70fd28d2.koyeb.app/api',
   DISABLE_BACKEND: false
 };
