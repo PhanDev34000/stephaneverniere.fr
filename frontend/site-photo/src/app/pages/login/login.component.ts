a9a711d9dcafefa3f66328e89894262e11f053a4
PhanDev34000 <vernierestephane@hotmail.com>
2025-09-12 18:16:10 +0200

    MAJ login

--- frontend/site-photo/src/app/pages/login/login.component.ts
+++ frontend/site-photo/src/app/pages/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../core/auth.service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule], // 👈 AJOUT
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
@@ -12,19 +15,22 @@ export class LoginComponent {
   identifiant = '';
   password = '';
   error: string | null = null;
+  loading = false; // 👈 AJOUT pour corriger l’erreur
 
   constructor(private auth: AuthService, private router: Router) {}
 
   onSubmit(form: NgForm) {
     if (form.invalid) return;
+    this.loading = true;
 
     this.auth.login(this.identifiant, this.password).subscribe({
       next: () => {
         this.error = null;
-        this.router.navigate(['/admin']); // redirige vers admin
+        this.router.navigate(['/admin']);
       },
       error: err => {
         this.error = err.error?.message || 'Échec de la connexion';
+        this.loading = false;
       }
     });
   }
