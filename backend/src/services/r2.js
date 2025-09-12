c03d758e0692aea62ba4f2c5025a992d6096f82b
PhanDev34000 <vernierestephane@hotmail.com>
2025-09-12 11:27:15 +0200

    Fix presigned URL credentials V2

--- backend/src/services/r2.js
+++ backend/src/services/r2.js
@@ -1,31 +1,13 @@
-const { S3RequestPresigner } = require("@aws-sdk/s3-request-presigner");
-const { HttpRequest } = require("@smithy/protocol-http");
-const { formatUrl } = require("@aws-sdk/util-format-url");
-const { parseUrl } = require("@smithy/url-parser");
-const { Hash } = require("@smithy/hash-node");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { PutObjectCommand } = require("@aws-sdk/client-s3");
 
 exports.getPresignedUrl = async (Key, ContentType="application/octet-stream") => {
-  const presigner = new S3RequestPresigner({
-    credentials: {
-      accessKeyId: process.env.R2_ACCESS_KEY_ID,
-      secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
-    },
-    region: "auto",
-    sha256: Hash.bind(null, "sha256"),
+  const command = new PutObjectCommand({
+    Bucket,
+    Key,
+    ContentType,
   });
 
-  const url = await presigner.presign(
-    new HttpRequest({
-      ...parseUrl(process.env.R2_ENDPOINT),
-      method: "PUT",
-      protocol: "https:",
-      path: `/${Bucket}/${Key}`,
-      headers: {
-        host: new URL(process.env.R2_ENDPOINT).host,
-        "content-type": ContentType,
-      },
-    })
-  );
-
-  return formatUrl(url);
+  const url = await getSignedUrl(client, command, { expiresIn: 3600 }); // 1h
+  return url;
 };
