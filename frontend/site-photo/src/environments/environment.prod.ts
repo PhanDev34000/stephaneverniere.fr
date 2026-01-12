255e0469d548d8fae6373940d9182d08ea21f044
PhanDev34000 <vernierestephane@hotmail.com>
2026-01-12 17:18:12 +0100

    maj environment.prod.ts

--- frontend/site-photo/src/environments/environment.prod.ts
+++ frontend/site-photo/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_BASE: 'https://fluffy-orel-svdev34-1caab703.koyeb.app',
+  API_BASE: 'https://fluffy-orel-svdev34-1caab703.koyeb.app/api',
   DISABLE_BACKEND: false
 };
